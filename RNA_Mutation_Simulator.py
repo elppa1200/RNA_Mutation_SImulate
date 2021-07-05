22081cd62e230e34329d4192e67bdb30db99e51a
Huere <56443524+elppa1200@users.noreply.github.com>
2021-07-05 13:55:16 +0900

    Update RNA_Mutation_Simulator.py

--- RNA_Mutation_Simulator.py
+++ RNA_Mutation_Simulator.py
@@ -59,18 +59,17 @@ def checkdna(dna_sequence):
 
 for i in range(int(ticks)):
     
-    if checkdna(dna_sequence) == 'err': #예외 처리
+    if checkdna(dna_sequence) == 'err':
         print("DNA contains errors: " + dna_sequence)
         break
     
     counter = counter + 1
-    print("[STAGE " + str(counter) + "]") #Epoch
+    print("[STAGE " + str(counter) + "]") 
     
-    epoch = dna_changeH(lendna) # haufigkeit는 돌연변이? 무슨 사건의 횟수 
-    #랜덤한 수 list를 받는 걸 보니 각 DNA에 변이 일으키는 함수일듯 
+    epoch = dna_changeH(lendna)
     print("Epoch:" + str(epoch))
     
-    for i in range(epoch): # 변이 횟수만큼
+    for i in range(epoch)
         dnaID(lendna)
     
     print('Change Index : ' + str(change_IDs))
@@ -79,7 +78,6 @@ for i in range(int(ticks)):
         ch_Index.append(dna_sequence[change_IDs[i]])
 
     print('Changed : ' + str(ch_Index))
-
     
     for i in range(epoch):
 
@@ -135,9 +133,6 @@ for i in range(int(ticks)):
             else : 
                 dna_sequence = DNA_part1 + changeInto() + DNA_part2
     
-            
-        
-
         if init_dna[i] != dna_sequence[i] :
             a.append(dna_sequence[i])
     print("")
@@ -159,6 +154,3 @@ print('initial rna : '+ init_dna + '\nchanged rna : ' + dna_sequence)
 
 print('everage : '+str(everage)+'\nU : ' + str(round(everage_U, 1))+'\nG : ' + str(round(everage_G, 1))+'\nC : ' + str(round(everage_C, 1))+'\nT : ' + str(round(everage_C, 1)) + '\nA : ' + str(round(everage_A, 1)))
 print('total percentage : ' + str(percen))
-
-
-# epoch 5000개 주고 한 epoch당 초기 dna랑 변경 dna 대조해서 달라진 dna 추출한 다음에 달라지는 경향 조사 
